9fb153f2bf5989e1a4e1f6b5cad932c805077a14
poppy california <johannnhandoko@gmail.com>
2025-04-24 17:06:35 +0200

    done

--- src/pages/StudentDetailsPage.jsx
+++ src/pages/StudentDetailsPage.jsx
@@ -1,19 +1,18 @@
 import placeholderImage from "./../assets/placeholder.png";
 import studentsData from "./../assets/students.json";
+import { Link } from "react-router-dom";
 
 function StudentDetailsPage() {
-
-  // Find the current student profile by id.
-  // In this case, the student with id 1. The `studentId` is hard-coded for now.
-  // This could be a URL parameter from React Router, e.g. /students/:studentId
   const studentId = "1";
-  const studentProfile = studentsData.find((student) => student._id === studentId);
+  const studentProfile = studentsData.find(
+    (student) => student._id === studentId
+  );
 
   return (
     <div className="StudentDetailsPage bg-gray-100 py-6 px-4 border-2 border-fuchsia-500 m-2">
       <h1>Student Details Page</h1>
       <div className="bg-white p-8 rounded-lg shadow-md mb-6">
-        {studentProfile && (
+        {studentProfile ? (
           <>
             <img
               src={studentProfile.image || placeholderImage}
@@ -65,13 +64,15 @@ function StudentDetailsPage() {
               </p>
             </div>
 
-
-            {/* Back button */}
-            <button className="text-white px-4 py-2 rounded bg-green-500 hover:bg-green-600 transition duration-300 ease-in-out">
+            <Link
+              to="/"
+              className="inline-block text-white px-4 py-2 rounded bg-green-500 hover:bg-green-600 transition duration-300 ease-in-out"
+            >
               Back
-            </button>
-
+            </Link>
           </>
+        ) : (
+          <p>Student not found</p>
         )}
       </div>
     </div>
